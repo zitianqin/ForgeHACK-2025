5c12ad712eb69852b2c7a18dbcea70c7dc27d44a
Zitian Qin <zitianqin2015@gmail.com>
2025-02-10 16:47:29 +1100

    Update permissions

--- FindTeamInfo/src/index.js
+++ FindTeamInfo/src/index.js
@@ -1,3 +1,16 @@
-export function findSlackChannel(payload) {
+import api, { route } from "@forge/api";
+
+export async function findSlackChannel(payload) {
   console.log(`Logging message: ${payload.teamName}`);
+
+  const response = await api
+    .asUser()
+    .requestConfluence(route`/wiki/api/v2/pages`, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+  console.log(`Response: ${response.status} ${response.statusText}`);
+  console.log(await response.json());
 }
