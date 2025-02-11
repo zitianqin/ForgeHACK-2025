6f26f799ebc6ba2f9d2625abc2cfdc68d9a6f19f
Freddie Lu <fredericklu@student.unsw.edu.au>
2025-02-11 21:00:57 +1100

    food connect almost done

--- DeliverFood/src/index.js
+++ DeliverFood/src/index.js
@@ -5,97 +5,93 @@ import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
-const ACCESS_KEY = "a97697f0-6bc6-4377-b5f9-6fa832df8135"
+const ACCESS_KEY = "783100bb-8615-439e-916e-24c3a7ccd32d"
 const DEVELOPER_ID = "56956d93-73d0-4313-a245-9e9eb9d13190"
-const SIGNING_SECRET = "LeTLk6GWOzzqE3qmqn52OZL0DoX1XABNuDgluI3yrDg"
+const SIGNING_SECRET = "71zEFoQQbZnVwZprLJwQpbVxY7b35tA2B4DFQSg1TT8"
 
-const accessKey = {
-  developer_id: DEVELOPER_ID,
-  key_id: ACCESS_KEY,
-  signing_secret: SIGNING_SECRET
+export async function order(payload) {
+  console.log(payload);
+
+  const accessKey = {
+    developer_id: DEVELOPER_ID,
+    key_id: ACCESS_KEY,
+    signing_secret: SIGNING_SECRET
+  }
+  
+  const data = {
+    aud: 'doordash',
+    iss: accessKey.developer_id,
+    kid: accessKey.key_id,
+    exp: Math.floor(Date.now() / 1000 + 300),
+    iat: Math.floor(Date.now() / 1000),
+  }
+  
+  const headers = { algorithm: 'HS256', header: { 'dd-ver': 'DD-JWT-V1' } }
+  
+  const token = jwt.sign(
+    data,
+    Buffer.from(accessKey.signing_secret, 'base64'),
+    headers,
+  )
+  
+  console.log(token)
+  
+  let str = uuidv4();
+  console.log(uuidv4());
+  const body = JSON.stringify({
+    external_delivery_id: str,
+    pickup_address: '901 Market Street 6th Floor San Francisco, CA 94103',
+    pickup_business_name: 'Wells Fargo SF Downtown',
+    pickup_phone_number: '+16505555555',
+    pickup_instructions: 'Enter gate code 1234 on the callbox.',
+    dropoff_address: '901 Market Street 6th Floor San Francisco, CA 94103',
+    dropoff_business_name: 'Wells Fargo SF Downtown',
+    dropoff_phone_number: '+16505555555',
+    dropoff_instructions: 'Enter gate code 1234 on the callbox.',
+    order_value: 1999,
+  })
+  
+  // axios
+  //   .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
+  //     headers: {
+  //       Authorization: 'Bearer ' + token,
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //   .then(function (response) {
+  //     console.log(response.data)
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error)
+  //   })
+  
+  await axios
+    .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      validateStatus: false, // Allows us to capture the full error response
+    })
+    .then((response) => {
+      if (response.status === 400) {
+        console.error("🚨 Validation Error:", response.data);
+      } else {
+        
+        console.log("✅ Success:", response.data);
+        const ans = response.data.tracking_url;
+        console.log(ans);
+        return {tracking_url: ans};
+      }
+    })
+    .catch((error) => {
+      console.error("❌ Error Response:", error.response ? error.response.data : error.message);
+    });
+
 }
 
-// console.log(accessKey)
+order({});
 
-const data = {
-  aud: 'doordash',
-  iss: accessKey.developer_id,
-  kid: accessKey.key_id,
-  exp: Math.floor(Date.now() / 1000 + 300),
-  iat: Math.floor(Date.now() / 1000),
-}
-
-const headers = { algorithm: 'HS256', header: { 'dd-ver': 'DD-JWT-V1' } }
-
-const token = jwt.sign(
-  data,
-  Buffer.from(accessKey.signing_secret, 'base64'),
-  headers,
-)
-
-console.log(token)
-
-// const body = JSON.stringify({
-//   external_delivery_id: 'D-12345',
-//   pickup_address: '123 Pitt St, Sydney NSW 2000',
-//   pickup_business_name: 'OOSHMAN Maroubra',
-//   pickup_phone_number: '+61412345678',
-//   pickup_instructions: 'Back of restaurant',
-//   dropoff_address: '363 George St, Sydney NSW 2000',
-//   dropoff_business_name: 'Atlassian',
-//   dropoff_phone_number: '+61412345679',
-//   dropoff_instructions: 'Give Lara a call',
-//   order_value: 59999,
-// })
-
-
-let str = uuidv4();
-console.log(uuidv4());
-const body = JSON.stringify({
-  external_delivery_id: str,
-  pickup_address: '4 Lang St, Sydney NSW 2000',
-  pickup_business_name: 'Dominos',
-  pickup_phone_number: '+16505555555',
-  pickup_instructions: 'Enter gate code 1234 on the callbox.',
-  dropoff_address: '27/363 George St, Sydney NSW 2000',
-  dropoff_business_name: 'Wells Fargo SF Downtown',
-  dropoff_phone_number: '+16505555555',
-  dropoff_instructions: 'Enter gate code 1234 on the callbox.',
-  order_value: 1999,
-})
-
-// axios
-//   .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
-//     headers: {
-//       Authorization: 'Bearer ' + token,
-//       'Content-Type': 'application/json',
-//     },
-//   })
-//   .then(function (response) {
-//     console.log(response.data)
-//   })
-//   .catch(function (error) {
-//     console.log(error)
-//   })
-
-axios
-  .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    validateStatus: false, // Allows us to capture the full error response
-  })
-  .then((response) => {
-    if (response.status === 400) {
-      console.error("🚨 Validation Error:", response.data);
-    } else {
-      console.log("✅ Success:", response.data);
-    }
-  })
-  .catch((error) => {
-    console.error("❌ Error Response:", error.response ? error.response.data : error.message);
-  });
 
 
 
